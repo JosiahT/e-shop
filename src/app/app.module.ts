a796d90c08ce7a6111f90b0dd9549e08876c23a5
Eyosiyas Tadesse <e.tadesse@lb.lashback.com>
2018-05-15 17:01:32 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    MenuNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'shopping-cart', component: ShoppingCartComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
